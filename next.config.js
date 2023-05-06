4ac62a41130c9a191f0920cb04a14c5aa715cbc7
Kenji Saito <ken-yo@mbr.nifty.com>
2023-05-06 15:15:21 +0900

    Migrade to Next.13.4

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const config =  {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   cleanDistDir: true,
   images: {
     unoptimized: true,
+  },
+  experimental: {
+    appDir: true,
   },
 };
 
---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export default function Document({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
