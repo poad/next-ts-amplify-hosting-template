30f5929b82764fe28e4d59cbac269446f900f315
Kenji Saito <ken-yo@mbr.nifty.com>
2023-05-15 09:04:24 +0900

    Bumps node modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export default function Document({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
   );
