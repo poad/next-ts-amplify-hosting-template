0b00c95c7f1d46dc154bcc09f2a5c8d1b32339fc
Kenji Saito <ken-yo@mbr.nifty.com>
2023-08-09 08:12:47 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -21,21 +21,21 @@ if [ $result -ne 0 ]; then
   exit $result
 fi
 
-# cd "${CURRENT}/cdk"
-# git pull --prune
-# result=$?
-# if [ $result -ne 0 ]; then
-#   cd "${CUR}"
-#   exit $result
-# fi
-# echo ""
-# pwd
-# rm -rf node_modules && pnpm install && pnpm dedupe && pnpm up && pnpm lint-fix && pnpm build
-# result=$?
-# if [ $result -ne 0 ]; then
-#   cd "${CUR}"
-#   exit $result
-# fi
+cd "${CURRENT}/cdk"
+git pull --prune
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+echo ""
+pwd
+rm -rf node_modules && pnpm install && pnpm dedupe && pnpm up && pnpm lint-fix && pnpm build
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
 git commit -am "Bumps node modules" && git push
 result=$?
 if [ $result -ne 0 ]; then
