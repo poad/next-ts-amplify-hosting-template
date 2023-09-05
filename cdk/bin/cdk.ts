388c13b9782532224ce7ccc9e39ee2bc02ed7176
Kenji Saito <ken-yo@mbr.nifty.com>
2023-09-05 23:31:08 +0900

    Migrate to biome

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import * as cdk from "aws-cdk-lib";
+import "source-map-support/register";
+import { CdkStack } from "../lib/cdk-stack";
 
 const app = new cdk.App();
 const stackName =
-  app.node.tryGetContext('stack_name') || 'next-ts-amplify-hosting-stack';
-const appName = app.node.tryGetContext('app_name') || 'AmplifyApp';
+	app.node.tryGetContext("stack_name") || "next-ts-amplify-hosting-stack";
+const appName = app.node.tryGetContext("app_name") || "AmplifyApp";
 new CdkStack(app, stackName, {
-  appName,
+	appName,
 });
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,32 +1,32 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as assets from 'aws-cdk-lib/aws-s3-assets';
-import * as amplify from '@aws-cdk/aws-amplify-alpha';
+import * as amplify from "@aws-cdk/aws-amplify-alpha";
+import * as cdk from "aws-cdk-lib";
+import * as assets from "aws-cdk-lib/aws-s3-assets";
+import { Construct } from "constructs";
 
 interface CdkStackProps extends cdk.StackProps {
-  appName: string;
+	appName: string;
 }
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: CdkStackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props: CdkStackProps) {
+		super(scope, id, props);
 
-    const { appName } = props;
+		const { appName } = props;
 
-    const asset = new assets.Asset(this, 'Asset', {
-      path: '../out',
-    });
+		const asset = new assets.Asset(this, "Asset", {
+			path: "../out",
+		});
 
-    const amplifyApp = new amplify.App(this, 'AmplifyApp', {
-      appName,
-    });
+		const amplifyApp = new amplify.App(this, "AmplifyApp", {
+			appName,
+		});
 
-    const main = amplifyApp.addBranch('main', {
-      asset: asset,
-    });
+		const main = amplifyApp.addBranch("main", {
+			asset: asset,
+		});
 
-    new cdk.CfnOutput(this, 'AmplifyAppURL', {
-      value: `https://${main.branchName}.${amplifyApp.appId}.amplifyapp.com`,
-    });
-  }
+		new cdk.CfnOutput(this, "AmplifyAppURL", {
+			value: `https://${main.branchName}.${amplifyApp.appId}.amplifyapp.com`,
+		});
+	}
 }
